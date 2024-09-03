e6c215a9deed26ccdb5b16feae96e9e8611e253b
hunter666hz <hzhunter7@gmail.com>
2024-09-03 20:49:01 +0100

    modified:   app/journals/page.tsx 	modified:   package-lock.json 	modified:   package.json

--- app/journals/page.tsx
+++ app/journals/page.tsx
@@ -2,25 +2,37 @@
 import { useSearchParams } from "next/navigation";
 import { RetrievePages } from "../lib/action";
 import { useEffect, useState } from "react";
+import { CiPen } from "react-icons/ci";
+import { FaRegTrashCan } from "react-icons/fa6";
+import { BiTrash } from "react-icons/bi";
 
 export default  function Journals() { 
     const searchParams = useSearchParams();
   const message = searchParams.get('message');
   const [Pages,setPages] =useState([])
+  const [Loading , setLoading] = useState<boolean>(true)
   useEffect(() => {
     const fetchData = async () => {
-      try {
-        const data = await RetrievePages();
-        setPages(data);
-      } catch (error) {
-        console.error("Failed to fetch pages:", error);
-      }
+        try {
+            const data = await RetrievePages();
+            setPages(data);
+        } catch (error) {
+            console.error("Failed to fetch pages:", error);
+        } finally { 
+            setLoading(false)
+        }
     };
 
       fetchData();
       console.log(Pages)
   }, []);
-    return (<div className="h-screen">
+    if (Loading) {
+        return (
+            <p>laoding ..........</p>
+        );
+     } else { 
+
+        return (<div className="h-screen">
 
         <h1 className=" block text-center text-4xl bg-green-400 w-1/2 h-2/6 pt-8 pb-8   mr-auto ml-auto mb-6  mt-6 shadow-2xl">
             journal created suc
@@ -28,7 +40,7 @@ export default  function Journals() {
         <h1 className="text-red-500 text-center text-6xl mb-5">this all the journals you have</h1>
         <div className="flex flex-col  h-screen items-center">
                 {Pages.map((e:any, i:any) => (
-                    <div className="w-4/5 min-h-32 text-center border border-white  m-2 rounded-xl flex "> 
+                    <a className="w-4/5 min-h-32 text-center border border-white  m-2 rounded-xl flex "> 
                     <div className="flex flex-col flex-grow">
                         
                 <div className="h-2/6 flex justify-center items-center ">
@@ -43,11 +55,19 @@ export default  function Journals() {
 
                 </div> 
                     </div>
-                    <div className="w-[10%] bg-purple-500 flex flex-col">
-                        <div className="flex-grow bg-yellow-200"></div>
-                        <div className="flex-grow bg-orange-200"></div>
+                    <div className="w-[10%] flex flex-col">
+                        <div className="flex-grow  flex items-center justify-center">
+                                <a href="" className="flex items-center justify-center">
+                            <CiPen  size={"50%"} className="text-white transition-all hover:size-3/5 hover:text-red-400 "/>
+                                </a>
+                        </div>
+                            <div className="flex-grow  flex items-center justify-center">
+                                <a href="http://" className="flex items-center justify-center">
+                            <FaRegTrashCan size={"40%"} className="text-white transition-all hover:size-1/2 hover:text-red-500 "  />
+                                </a>
+                        </div>
                     </div>
-            </div>
+            </a>
                 )) }
         <div className="w-4/5 min-h-48 text-center bg-red-400  m-2 rounded-md flex "> 
                 <div className="flex flex-col flex-grow">
@@ -83,4 +103,5 @@ export default  function Journals() {
         </div>
     </div>
     );
+}
 }
